aece9b77cd29610a1af81fb645b4c4b37684fb11
Patrick Franz <clymbing@gmail.com>
2016-12-19 18:07:02 -0800

    Finished day 3

--- projects/Day3-UpdateCSSVar/js/css-var.js
+++ projects/Day3-UpdateCSSVar/js/css-var.js
@@ -1,1 +1,14 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('#controls input');
+const bodyStyles = window.getComputedStyle(document.body);
+
+
+function handleUpdate(){
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  document.getElementById('current-size').innerHTML = bodyStyles.getPropertyValue('--size');
+  document.getElementById('current-blur').innerHTML = bodyStyles.getPropertyValue('--blur');
+  document.getElementById('current-color').innerHTML = bodyStyles.getPropertyValue('--bg-color');
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
